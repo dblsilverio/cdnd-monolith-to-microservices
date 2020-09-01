b8525fe800082b84185b0f1d1e3190d2497c05ee
Diogo Silvério <dblsilverio+github@gmail.com>
2020-09-01 00:23:45 -0300

    Correcting image version
    Using TRAVIS_BUILD_NUMBER instead of TRAVIS_BUILD_Id.

--- udagram-reverseproxy/build.sh
+++ udagram-reverseproxy/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --tag diogosilverio/udagram-reverseproxy:${TRAVIS_BUILD_ID:-latest} .
+docker build --tag diogosilverio/udagram-reverseproxy:${TRAVIS_BUILD_NUMBER:-latest} .
--- udagram-reverseproxy/push.sh
+++ udagram-reverseproxy/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker push diogosilverio/udagram-reverseproxy:${TRAVIS_BUILD_ID:-latest}
+docker push diogosilverio/udagram-reverseproxy:${TRAVIS_BUILD_NUMBER:-latest}
